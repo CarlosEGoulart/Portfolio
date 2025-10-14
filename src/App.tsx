15ac31143af4cb7d84b48616c285daee809eb122
goulartoliveiracarloseduardo <goulartoliveiracarloseduardo@gmail.com>
2025-10-14 19:45:54 +0000

    LandingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Header from './components/Header/index.tsx'
-import Footer from './components/Footer/index.tsx'
-import Main from './components/Main.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
 
 export default function App() {
   return (
-    <div className="bg-gray-800 text-white min-h-screen flex flex-col items-center justify-center">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import Header from '../components/Header/index'
+import Main from '../components/Main'
+
+export const Home = () => {
+    return <div className='text-white bg-black min-h-screen flex flex-col'>
+        <Header />
+        <Main />
+    </div>
+};
