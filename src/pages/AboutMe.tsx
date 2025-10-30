dcec262c20a693bd46aa43d2e2155a04889103b5
carlosegoulart <goulart193@gmail.com>
2025-10-29 21:51:30 -0300

    AboutMeUpd

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,7 +1,7 @@
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function AboutMe() {
-    return <div className="flex flex-row items-center justify-center gap-75 h-screen bg-black text-white px-20">
+    return <div className="flex flex-row items-center justify-center gap-75 h-screen bg-grey-800 text-white px-20">
         <div className="flex flex-col gap-5">
             <h1 className="text-4xl max-w-xl font-bold mb-8">Carlos Eduardo Goulart Oliveira</h1>
             <div className="text-start">
@@ -18,7 +18,7 @@ export default function AboutMe() {
         </div>
 
         <div className="w-full max-w-md">
-            <form className="bg-grey-800 shadow-md rounded px-8 pt-6 pb-12 mb-">
+            <form className="bg-stone-800 shadow-md rounded px-8 pt-6 pb-12 mb-10 gap-5">
                 <h2 className="text-2xl font-bold mb-6 text-center">Envie sua mensagem</h2>
                 <div className="mb-4">
                     <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="email">
@@ -38,8 +38,8 @@ export default function AboutMe() {
                     </label>
                     <textarea className="shadow appearance-none border rounded w-full py-2 px-3 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline h-24" id="message" placeholder="Sua mensagem"></textarea>
                 </div>
-                <div className="flex items-center justify-center">
-                    <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 pb-8 rounded focus:outline-none focus:shadow-outline" type="button">
+                <div className="flex items-center justify-center pb-5">
+                    <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                         Enviar
                     </button>
                 </div>
